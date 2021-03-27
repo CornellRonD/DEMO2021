e59804c9063b6e44879a5152e1b99bfc86ed4fef
Ron DiNapoli <rd29@cornell.edu>
2021-03-27 09:21:53 -0700

    Start help system

--- coolscript.sh
+++ coolscript.sh
@@ -2,3 +2,8 @@
 # coolscript.sh
 
 echo "Welcome to coolscript.sh!"
+
+if [[ "$1" == "help" ]]; then
+    echo "coolscript: help"
+    echo "This command will show you how cool you are, just run it"
+fi
